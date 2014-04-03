556339971b217038d987b0990e12ad036f5eb225
Erik Tehler <erik@tehler.se>
2014-04-03 10:14:24 +0200

    fix

---
+++ config.prod.php
@@ -0,0 +1,11 @@
+<?php
+	define("DATABASE_HOST",	"127.0.0.1");
+	define("DATABASE_USERNAME",	"root");
+	define("DATABASE_PASSWORD",	"piZZakoLLektivet");
+	define("DATABASE_DATABASE",	"supranking");
+
+	define("ADMIN_USERNAME", "admin");
+	define("ADMIN_PASSWORD", "d033e22ae348aeb5660fc2140aec35850c4da997");
+	
+	define("COMPETITIONS_ADDED_TO_SUM", 6);
+?>
